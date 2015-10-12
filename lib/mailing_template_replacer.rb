9eb42ad25175d4e2cc8da4f9619ab571a960b22d
JewelSam <mrswlasowa@gmail.com>
2015-10-12 21:03:35 +0300

    Move method replace_tokens_in_email in MailingTemplateReplacer to class methods

--- lib/mailing_template_replacer.rb
+++ lib/mailing_template_replacer.rb
@@ -2,7 +2,7 @@ require 'token_presenter_factory'
 require 'token_presenters/string_token_presenter'
 
 class MailingTemplateReplacer
-  def replace_tokens_in_email(body, tokens)
+  def self.replace_tokens_in_email(body, tokens)
     tokens.each do |token|
       token_presenter = TokenPresenterFactory.get_presenter(token.type)
       body.gsub!(/%{#{token.name}}/, token_presenter.format_token(token))
--- spec/mailing_template_replacer_spec.rb
+++ spec/mailing_template_replacer_spec.rb
@@ -5,11 +5,10 @@ RSpec.describe MailingTemplateReplacer do
   describe '::replace_tokens_in_email' do
     # как это протестировать? передавать разные токены? или как?
     it 'should replace tokens in body' do
-      mailing_template_replacer = MailingTemplateReplacer.new
       body = "Здравствуйте, %{user_fio}!"
       tokens = [UserFioToken.new('fio')]
 
-      replaced_body = mailing_template_replacer.replace_tokens_in_email(body, tokens)
+      replaced_body = MailingTemplateReplacer.replace_tokens_in_email(body, tokens)
 
       expect(replaced_body).to eq("Здравствуйте, fio!")
     end
