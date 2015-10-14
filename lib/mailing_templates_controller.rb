338c51054a79d419c1c040f5461b6822a1c914f0
JewelSam <mrswlasowa@gmail.com>
2015-10-14 09:31:32 +0300

    Add birthday email action to mailing template controller

--- lib/mailing_templates_controller.rb
+++ lib/mailing_templates_controller.rb
@@ -19,4 +19,18 @@ class MailingTemplatesController
     # InviteUserMailer.deliver(user_email, subject, replaced_body)
 
   end
+
+  # повторение метода confirm_email, что с этим делать?
+  def birthday_email(template_type, user_id, promo_code_id, repositories)
+
+    template = repositories[:mailing_templates].find(template_type)
+
+    mail_tokens_extractor = MailTokensExtractorFactory.find_by_type(template_type)
+
+    options = {user_id: user_id, promo_code: promo_code_id}
+    mail_tokens_list = mail_tokens_extractor.prepare_tokens(options, repositories)
+
+    replaced_body = MailingTemplateReplacer.replace_tokens_in_email(template, mail_tokens_list)
+
+  end
 end
--- spec/mailing_templates_controller_spec.rb
+++ spec/mailing_templates_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'mailing_templates_controller.rb'
+require 'repositories/users_repository.rb'
 
 RSpec.describe MailingTemplatesController do
+  templates_repository = MailingTemplatesRepository.new
+
+  users_repository = UsersRepository.new
+  users_repository.put(1, 'fio', 'from@example.com')
+
   describe '::confirm_email' do
     context 'when email is sent' do
       it 'should send with replaced body' do
-        templates_repository = MailingTemplatesRepository.new
+
         body = 'Здравствуйте, %{user_fio}! Ваш e-mail %{user_email} был указан при регистрации. ' +
                 'Перейдите по <a href="%{confirmation_link}">ссылке</a> для подтверждения.'
         templates_repository.put('confirm_email', body)
-
-        users_repository = UsersRepository.new
-        users_repository.put(1, 'fio', 'from@example.com')
 
         template_type = 'confirm_email'
         user_id = 1
@@ -29,6 +32,48 @@ RSpec.describe MailingTemplatesController do
                 'Перейдите по <a href="http://example.com">ссылке</a> для подтверждения.')
 
         # почти полное повторение mailing_template_replacer_spec, что с этим делать?
+      end
+    end
+  end
+
+
+  describe '::birthday_email' do
+    context 'when email is sent' do
+      it 'should send with replaced body' do
+
+        body = 'Здравствуйте, %{user_fio}! ' +
+                  'В этот замечательный день мы дарим вам скидку %{promo_code_value} по промо-коду: ' +
+                  '%{promo_code_code}. Скидка по промокоду будет действовать %{promo_code_duration}.'
+        templates_repository.put('birthday_email', body)
+
+        users_repository = UsersRepository.new
+        users_repository.put(1, 'fio', 'from@example.com')
+
+        promo_codes_repository = PromoCodesRepository.new
+        promo_code_start = Date.today
+        promo_code_finish = promo_code_start + 15
+        promo_codes_repository.put('code', 1000, promo_code_start, promo_code_finish)
+
+        template_type = 'birthday_email'
+        user_id = 1
+        promo_code = 'code'
+        repositories = {
+          mailing_templates: templates_repository,
+          users: users_repository,
+          promo_codes: promo_codes_repository
+        }
+
+        mailing_templates_controller = MailingTemplatesController.new
+        replaced_body = mailing_templates_controller.birthday_email(
+          template_type,
+          user_id,
+          promo_code,
+          repositories
+        )
+
+        expect(replaced_body).to eq('Здравствуйте, fio! ' +
+                  'В этот замечательный день мы дарим вам скидку 1000 руб по промо-коду: ' +
+                  'code. Скидка по промокоду будет действовать 15 дней.')
       end
     end
   end
